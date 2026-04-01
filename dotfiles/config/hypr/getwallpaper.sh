e34a8831d7da69568c42e05a6238b7de31bc5fd8
NekoTintin <tintin361official@gmail.com>
2026-04-02 00:56:28 +0200

    Remove All

--- dotfiles/config/hypr/getwallpaper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wall=$(swww query | awk '{print $8}' | tail -n1)
-
-echo "$wall" 
--- dotfiles/config/hypr/scripts/cava-bg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if ps aux | grep '[k]itty-bg'; then
-    pkill cava
-else
-    KITTY_DISABLE_WAYLAND=1 kitty --config ~/.config/kitty/kitty-bg.conf --class kitty-bg -e cava
-fi
